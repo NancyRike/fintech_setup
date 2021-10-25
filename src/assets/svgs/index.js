dca817ac4bd22b7047c172ee0dc6591f61d2125e
MayorErnest <eobumma@gmail.com>
2021-10-25 13:15:33 +0100

    ui: add dropdown

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,5 +1,6 @@
 import { ReactComponent as Clock } from "./clock.svg";
 import { ReactComponent as UploadCloud } from "./upload-cloud.svg";
 import { ReactComponent as WarningIcon } from "./warning.svg";
+import { ReactComponent as ArrowDown } from "./arrowDown.svg";
 
-export { Clock, UploadCloud, WarningIcon };
+export { Clock, UploadCloud, WarningIcon, ArrowDown };
---
+++ src/components/dropdown/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { ArrowDown } from "../../assets";
+
+import styles from "./styles.module.css";
+
+const Dropdown = ({ placeHolder, items }) => {
+  const [showList, setShowList] = useState(false);
+  const [currentValue, setCurrentValue] = useState(null);
+
+  const handleShowList = () => {
+    setShowList(!showList);
+  };
+
+  const handleCurrentValue = ({ target: { value } }) => {
+    setCurrentValue(value);
+    setShowList(false);
+  };
+
+  return (
+    <div className={`${styles.container}`}>
+      <div className={`${styles.header} py-2`} onClick={handleShowList}>
+        <div>
+          <span>{currentValue ? currentValue : placeHolder}</span>
+          <ArrowDown
+            className={styles.arrow}
+            style={{ transform: showList ? "rotate(180deg)" : "" }}
+          />
+        </div>
+      </div>
+      {showList ? (
+        <div className={`${styles.list} mt-3 p-3`}>
+          <ul>
+            {items
+              ? items.map((item, index) => (
+                  <li key={index + 2} className="my-2">
+                    <button
+                      className="px-1"
+                      value={item}
+                      onClick={handleCurrentValue}
+                    >
+                      {item}
+                    </button>
+                  </li>
+                ))
+              : ""}
+          </ul>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export { Dropdown };
