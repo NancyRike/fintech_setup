65af90a020a3f666f17db30175f1928ab72d37bd
MayorErnest <eobumma@gmail.com>
2021-11-01 10:46:16 +0100

    fix: adjust logo to route to home

--- src/layouts/authLayout/index.js
+++ src/layouts/authLayout/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { logo } from "../../assets";
 
 import styles from "./styles.module.css";
@@ -11,8 +12,10 @@ const AuthLayout = ({ children, authBgA }) => {
       }`}
     >
       <header className="container-fluid">
-        <div className="ms-4 py-4">
-          <img src={logo} alt="logo" />
+        <div className="ms-1 py-4">
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
         </div>
       </header>
       {children}
